99c2b4c71f920e7758afb3b0b1f8137c343ac050
Stanislav Lemeshaev <s.lemeshaev@haulmont.com>
2021-03-24 21:35:39 +0300

    files for project

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Станислав Лемешаев on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
